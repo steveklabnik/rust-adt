83a0205ae17dfcb4a36add80ec9198f4b917326b
Nikita Pekin <contact@nikitapek.in>
2014-08-27 16:33:35 +0000

    Write additional ArrayStack tests

--- src/stack.rs
+++ src/stack.rs
@@ -1,9 +1,15 @@
 /// TODO
 pub trait Stack<T> {
+    /// TODO
     fn push(&mut self, elem: T);
+    /// TODO
     fn pop(&mut self) -> Option<T>;
+    /// TODO
     fn peek(&self) -> Option<&T>;
+    /// TODO
     fn is_empty(&self) -> bool;
+    /// TODO
+    fn size(&self) -> uint;
 }
 
 /// TODO
@@ -40,6 +46,11 @@ impl<T> Stack<T> for ArrayStack<T> {
     fn is_empty(&self) -> bool {
         self.elements.is_empty()
     }
+
+    /// TODO
+    fn size(&self) -> uint {
+        self.elements.len()
+    }
 }
 
 #[cfg(test)]
@@ -50,23 +61,95 @@ mod tests {
     };
 
     #[test]
-    fn test_new() {
+    fn test_new_array_stack() {
         // Initialize the test stack.
         let stack: ArrayStack<int> = ArrayStack::new();
 
-        // Manually construct the Stack object expected from Stack::new().
-        let expected_stack: ArrayStack<int> = ArrayStack { elements: Vec::new() };
-
-        // Verify that Stack::new() produces a new empty Stack instance.
-        //assert_eq!(stack, expected_stack);
+        // Verify that the stack is empty.
+        assert!(stack.is_empty());
+        assert_eq!(stack.size(), 0);
     }
 
     #[test]
-    fn test_push() {
+    fn test_push_array_stack() {
         // Initialize a test stack.
         let mut stack: ArrayStack<int> = ArrayStack::new();
 
-        // Push an element onto the stack.
-        stack.push(10);
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the stack contains 3 elements.
+        assert!(!stack.is_empty());
+        assert_eq!(stack.size(), 3);
+    }
+
+    #[test]
+    fn test_push_then_pop_array_stack() {
+        // Initialize a test stack.
+        let mut stack: ArrayStack<int> = ArrayStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the pop() function works correctly.
+        assert_eq!(stack.pop().unwrap(), 22);
+        assert_eq!(stack.size(), 2);
+    }
+
+    #[test]
+    fn test_push_then_peek_array_stack() {
+        // Initialize a test stack.
+        let mut stack: ArrayStack<int> = ArrayStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the peek() function works correctly.
+        assert_eq!(stack.peek().unwrap(), &22);
+        assert_eq!(stack.size(), 3);
+    }
+
+    #[test]
+    fn test_pop_until_empty_array_stack() {
+        // Initialize a test stack.
+        let mut stack: ArrayStack<int> = ArrayStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Pop the elements off the stack.
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        // Verify that the stack is now empty.
+        assert!(stack.is_empty());
+        assert_eq!(stack.size(), 0);
+    }
+
+    #[test]
+    fn test_pop_empty_array_stack() {
+        // Initialize a test stack.
+        let mut stack: ArrayStack<int> = ArrayStack::new();
+
+        // Pop a non-existing element off the stack.
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn test_peek_empty_array_stack() {
+        // Initialize a test stack.
+        let stack: ArrayStack<int> = ArrayStack::new();
+
+        // Peek a non-existing element from the stack.
+        assert_eq!(stack.peek(), None);
     }
 }
