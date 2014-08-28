90ba9f7bcc588b19664d00bd2c5ada79187b5f76
Nikita Pekin <contact@nikitapek.in>
2014-08-28 01:30:08 -0400

    Add stack example

--- examples/stack.rs
+++ examples/stack.rs
@@ -1,1 +1,22 @@
-fn main() { }
+extern crate adt;
+
+use adt::{
+    Stack,
+    VecStack
+};
+
+fn main() {
+    // Initialize a VecStack stack.
+    let mut stack: VecStack<int> = VecStack::new();
+
+    // Push some elements onto the stack.
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    // Pop some elements off the stack.
+    println!("{}", stack.pop());
+
+    // Peek some elements from the stack.
+    println!("{}", stack.peek());
+}
