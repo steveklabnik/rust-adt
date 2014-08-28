5b3ee6624812274d1b2e7a87daa9706d1d6c4f37
Nikita Pekin <contact@nikitapek.in>
2014-08-28 01:20:26 -0400

    Move test_stack to two separate files

--- tests/test_dlist_stack.rs
+++ tests/test_dlist_stack.rs
@@ -3,14 +3,14 @@ extern crate adt;
 #[cfg(test)]
 mod test {
     use adt::{
-        VecStack,
+        DListStack,
         Stack
     };
 
     #[test]
-    fn test_new_vec_stack() {
+    fn test_new_stack() {
         // Initialize the test stack.
-        let stack: VecStack<int> = VecStack::new();
+        let stack: DListStack<int> = DListStack::new();
 
         // Verify that the stack is empty.
         assert!(stack.is_empty());
@@ -18,9 +18,9 @@ mod test {
     }
 
     #[test]
-    fn test_push_vec_stack() {
+    fn test_push_stack() {
         // Initialize a test stack.
-        let mut stack: VecStack<int> = VecStack::new();
+        let mut stack: DListStack<int> = DListStack::new();
 
         // Push some elements onto the stack.
         stack.push(1);
@@ -33,9 +33,9 @@ mod test {
     }
 
     #[test]
-    fn test_push_then_pop_vec_stack() {
+    fn test_push_then_pop_stack() {
         // Initialize a test stack.
-        let mut stack: VecStack<int> = VecStack::new();
+        let mut stack: DListStack<int> = DListStack::new();
 
         // Push some elements onto the stack.
         stack.push(1);
@@ -48,9 +48,9 @@ mod test {
     }
 
     #[test]
-    fn test_push_then_peek_vec_stack() {
+    fn test_push_then_peek_stack() {
         // Initialize a test stack.
-        let mut stack: VecStack<int> = VecStack::new();
+        let mut stack: DListStack<int> = DListStack::new();
 
         // Push some elements onto the stack.
         stack.push(1);
@@ -63,9 +63,9 @@ mod test {
     }
 
     #[test]
-    fn test_pop_until_empty_vec_stack() {
+    fn test_pop_until_empty_stack() {
         // Initialize a test stack.
-        let mut stack: VecStack<int> = VecStack::new();
+        let mut stack: DListStack<int> = DListStack::new();
 
         // Push some elements onto the stack.
         stack.push(1);
@@ -83,18 +83,18 @@ mod test {
     }
 
     #[test]
-    fn test_pop_empty_vec_stack() {
+    fn test_pop_empty_stack() {
         // Initialize a test stack.
-        let mut stack: VecStack<int> = VecStack::new();
+        let mut stack: DListStack<int> = DListStack::new();
 
         // Pop a non-existing element off the stack.
         assert_eq!(stack.pop(), None);
     }
 
     #[test]
-    fn test_peek_empty_vec_stack() {
+    fn test_peek_empty_stack() {
         // Initialize a test stack.
-        let stack: VecStack<int> = VecStack::new();
+        let stack: DListStack<int> = DListStack::new();
 
         // Peek a non-existing element from the stack.
         assert_eq!(stack.peek(), None);
---
+++ tests/test_vec_stack.rs
@@ -0,0 +1,102 @@
+extern crate adt;
+
+#[cfg(test)]
+mod test {
+    use adt::{
+        Stack,
+        VecStack
+    };
+
+    #[test]
+    fn test_new_stack() {
+        // Initialize the test stack.
+        let stack: VecStack<int> = VecStack::new();
+
+        // Verify that the stack is empty.
+        assert!(stack.is_empty());
+        assert_eq!(stack.size(), 0);
+    }
+
+    #[test]
+    fn test_push_stack() {
+        // Initialize a test stack.
+        let mut stack: VecStack<int> = VecStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the stack contains 3 elements.
+        assert!(!stack.is_empty());
+        assert_eq!(stack.size(), 3);
+    }
+
+    #[test]
+    fn test_push_then_pop_stack() {
+        // Initialize a test stack.
+        let mut stack: VecStack<int> = VecStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the pop() function works correctly.
+        assert_eq!(stack.pop(), Some(22));
+        assert_eq!(stack.size(), 2);
+    }
+
+    #[test]
+    fn test_push_then_peek_stack() {
+        // Initialize a test stack.
+        let mut stack: VecStack<int> = VecStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Verify that the peek() function works correctly.
+        assert_eq!(stack.peek(), Some(&22));
+        assert_eq!(stack.size(), 3);
+    }
+
+    #[test]
+    fn test_pop_until_empty_stack() {
+        // Initialize a test stack.
+        let mut stack: VecStack<int> = VecStack::new();
+
+        // Push some elements onto the stack.
+        stack.push(1);
+        stack.push(43);
+        stack.push(22);
+
+        // Pop the elements off the stack.
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        // Verify that the stack is now empty.
+        assert!(stack.is_empty());
+        assert_eq!(stack.size(), 0);
+    }
+
+    #[test]
+    fn test_pop_empty_stack() {
+        // Initialize a test stack.
+        let mut stack: VecStack<int> = VecStack::new();
+
+        // Pop a non-existing element off the stack.
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn test_peek_empty_stack() {
+        // Initialize a test stack.
+        let stack: VecStack<int> = VecStack::new();
+
+        // Peek a non-existing element from the stack.
+        assert_eq!(stack.peek(), None);
+    }
+}
